f2c18a5b7dab9a9d02fedb3b281ce1ec7b1e0be4
Andrew Nichols <danweasel@gmail.com>
2015-02-06 11:26:34 +0700

    renames

--- cmd/humbug/humbug.go
+++ cmd/humbug/humbug.go
@@ -15,7 +15,7 @@ import (
 	"strings"
 
 	"github.com/frenata/gomua"
-	"github.com/frenata/gomua/send"
+	"github.com/frenata/gomua/msa"
 )
 
 // client handles common data as a user navigates the MUA.
@@ -206,7 +206,7 @@ func (c *client) input(exit chan bool) {
 			}
 			old := c.messages[num-1].(*gomua.Message)
 			reply := replyMessage(old, c.user)
-			send.Send(c.configFile, reply)
+			msa.Send(c.configFile, reply)
 			old.Flag("R")
 		case input == "exit", input == "x", input == "quit", input == "q":
 			exit <- true
